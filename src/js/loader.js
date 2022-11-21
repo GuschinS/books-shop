1f51d9139624102cc39c4f1c37de6fd506619582
guschins <guschins@gmail.com>
2022-11-21 10:06:44 +0300

    feat: add loader

---
+++ src/js/loader.js
@@ -0,0 +1,23 @@
+import json from "../books";
+import { CatalogCard } from "./catalog-card";
+
+async function getBooks() {
+  try {
+    renderProducts(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getBooks();
+
+function renderProducts(array) {
+  let fragment = new DocumentFragment();
+  array.forEach((data, index) => {
+    let card = new CatalogCard(data, index);
+    fragment.append(card.renderCard());
+  });
+  const cardsList = document.querySelector(".cards__list");
+  cardsList.append(fragment);
+  return cardsList;
+}
